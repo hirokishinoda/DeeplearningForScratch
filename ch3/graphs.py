198de34e13737c65b8c5539a2ecc2fcae62baf13
hirokishinoda <progac27jhisi@gmail.com>
2022-03-03 10:31:45 +0000

    ReLU関数を追加

--- ch3/graphs.py
+++ ch3/graphs.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import sigmoid, step_function
+from functions import relu, sigmoid, step_function
 
 if __name__ == '__main__':
     x = np.arange(-5.0, 5.0, 0.1)
     #y = step_function(x)
-    y = sigmoid(x)
+    #y = sigmoid(x)
+    y =  relu(x)
 
     plt.plot(x, y)
     plt.ylim(-0.1, 1.1)
