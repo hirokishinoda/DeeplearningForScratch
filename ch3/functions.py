4cdabc5aa5d9f33188a8bd2f2ef2fc04c0f77c25
hirokishinoda <progac27jhisi@gmail.com>
2022-03-03 09:02:15 +0000

    sigmoid関数の実装

--- ch3/functions.py
+++ ch3/functions.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def step_function(x):
     return (x > 0).astype(np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
--- ch3/graphs.py
+++ ch3/graphs.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import step_function
+from functions import sigmoid, step_function
 
 if __name__ == '__main__':
     x = np.arange(-5.0, 5.0, 0.1)
-    y = step_function(x)
+    #y = step_function(x)
+    y = sigmoid(x)
 
     plt.plot(x, y)
     plt.ylim(-0.1, 1.1)
