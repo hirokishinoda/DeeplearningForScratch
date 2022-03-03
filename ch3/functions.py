e0b65e41bd9447984a050d1f4dc14fd1418a9bf4
hirokishinoda <progac27jhisi@gmail.com>
2022-03-03 11:06:45 +0000

    順伝搬モデルの追加

--- ch3/functions.py
+++ ch3/functions.py
@@ -8,3 +8,14 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+def identity_function(x):
+    return x
+
+def softmax(x):
+    c = np.max(x) # オーバーフロー対策
+    exp_x = np.exp(x - c)
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
+
+    return y
