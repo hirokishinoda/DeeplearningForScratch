b95d7a92d5587785338801b652a8aed1694be6be
hirokishinoda <progac27jhisi@gmail.com>
2022-04-02 11:10:59 +0000

    4章の追加

---
+++ book2/ch4/train.py
@@ -0,0 +1,41 @@
+import pickle
+import sys
+sys.path.append('..')
+sys.path.append('../common')
+
+import numpy as np
+from common.util import create_contexts_target, to_gpu
+from common.trainer import Trainer
+from common.optimizers import Adam
+from cbow import CBOW
+from dataset import ptb 
+
+window_size = 5
+hidden_size =100
+batch_size = 100
+max_epoch = 1
+
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+
+contexts, target = create_contexts_target(corpus, window_size)
+GPU = False
+if GPU:
+    contexts, target = to_gpu(contexts), to_gpu(target)
+
+model = CBOW(vocab_size, hidden_size, window_size, corpus)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.plot()
+
+word_vecs = model.word_vecs
+
+params = {}
+params['word_vecs'] = word_vecs.astype(np.float16)
+params['word_to_id'] = word_to_id
+params['id_to_word'] = id_to_word
+pkl_file = 'cbow_params.pkl'
+with open(pkl_file, 'wb') as f:
+    pickle.dump(params, f, -1)
--- book2/common/layers.py
+++ book2/common/layers.py
@@ -21,6 +21,7 @@ class Relu:
 
 class Sigmoid:
     def __init__(self):
+        self.params, self.grads = [], []
         self.out = None
 
     def forward(self, x):
@@ -101,6 +102,28 @@ class SoftmaxWithLoss:
 
         return dx
 
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+
+        return dx
+
 class Convolution:
     def __init__(self, W, b, stride=1, pad=0):
         self.W = W
@@ -167,3 +190,23 @@ class Pooling:
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
         
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, =self.grads
+        dW[...] = 0
+        #dW[self.idx] = dout
+        np.add.at(dW, self.idx, dout)
+        
+        return None
